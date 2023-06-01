812db0504631dac6703a0403cbe7b4fb41bdcacc
Larissa <larisssa_rga@hotmail.com>
2023-06-01 15:58:44 -0300

    [add] - study_schedule - req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    count = 0
+    for start_time, end_time in permanence_period:
+        if type(start_time) != int\
+                or type(end_time) != int:
+            return None
+        if start_time <= target_time <= end_time:
+            count += 1
+    return count
